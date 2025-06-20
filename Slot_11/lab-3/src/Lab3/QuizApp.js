1f7414919a32fee618a18d03545306176c222f00
TKauseb2103 <tuankietlv2103.dev@gmail.com>
2025-06-20 13:04:17 +0700

    fix lab 3

--- Slot_11/lab-3/src/Lab3/QuizApp.js
+++ Slot_11/lab-3/src/Lab3/QuizApp.js
@@ -1,82 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Question from './Question';
 import Score from './Score';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class QuizApp extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            questions: [
-                {
-                    id: 1,
-                    question: "What is the capital of France?",
-                    options: ["Paris", "London", "Berlin", "Madrid"],
-                    answer: "Paris"
-                },
-                {
-                    id: 2,
-                    question: "What is the largest planet in our solar system?",
-                    options: ["Jupiter", "Saturn", "Mars", "Earth"],
-                    answer: "Jupiter"
-                },
-            ],
-            currentQuestion: 0,
-            score: 0,
-            quizEnd: false
-        };
-    }
+const QUESTIONS = [
+    {
+        id: 1,
+        question: "What is the capital of France?",
+        options: ["Paris", "London", "Berlin", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        id: 2,
+        question: "What is the largest planet in our solar system?",
+        options: ["Jupiter", "Saturn", "Mars", "Earth"],
+        answer: "Jupiter"
+    },
+];
 
-    handleAnswer = (selectedOption) => {
-        const { currentQuestion, questions, score } = this.state;
-        if (selectedOption === questions[currentQuestion].answer) {
-            this.setState({
-                score: score + 1
-            });
+function QuizApp() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [quizEnd, setQuizEnd] = useState(false);
+
+    useEffect(() => {
+        if (!quizEnd && currentQuestion === 0 && score === 0) {
+        }
+    }, [quizEnd, currentQuestion, score]);
+
+    const handleAnswer = (selectedOption) => {
+        if (selectedOption === QUESTIONS[currentQuestion].answer) {
+            setScore(prev => prev + 1);
         }
 
-        if (currentQuestion < questions.length - 1) {
-            this.setState({
-                currentQuestion: currentQuestion + 1
-            });
+        if (currentQuestion < QUESTIONS.length - 1) {
+            setCurrentQuestion(prev => prev + 1);
         } else {
-            this.setState({
-                quizEnd: true
-            });
+            setQuizEnd(true);
         }
-    }
+    };
 
-    resetQuiz = () => {
-        this.setState({
-            currentQuestion: 0,
-            score: 0,
-            quizEnd: false
-        });
-    }
+    const resetQuiz = () => {
+        setCurrentQuestion(0);
+        setScore(0);
+        setQuizEnd(false);
+    };
 
-    render() {
-        const { questions, currentQuestion, quizEnd, score } = this.state;
-
-        return (
-            <Container className="py-5">
-                {quizEnd ? (
-                    <Score 
-                        score={score} 
-                        totalQuestions={questions.length} 
-                        onReset={this.resetQuiz} 
-                    />
-                ) : (
-                    <Question 
-                        id={questions[currentQuestion].id}
-                        question={questions[currentQuestion].question}
-                        options={questions[currentQuestion].options}
-                        onAnswer={this.handleAnswer}
-                    />
-                )}
-            </Container>
-        );
-    }
+    return (
+        <Container className="py-5">
+            {quizEnd ? (
+                <Score
+                    score={score}
+                    totalQuestions={QUESTIONS.length}
+                    onReset={resetQuiz}
+                />
+            ) : (
+                <Question
+                    id={QUESTIONS[currentQuestion].id}
+                    question={QUESTIONS[currentQuestion].question}
+                    options={QUESTIONS[currentQuestion].options}
+                    onAnswer={handleAnswer}
+                />
+            )}
+        </Container>
+    );
 }
 
 export default QuizApp;
